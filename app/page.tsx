4f4042092249eb93b6f9239ac38f86de48d6acb7
Nicobonilla2 <nicolas.bonilla@alumnos.usm.cl>
2024-03-19 15:19:52 -0300

    Add mercadosSideEffect

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,24 @@
 import Navbar from "./components/navbar/Navbar";
 import CarouselHome from "./components/carousel/CarouselHome";
 import Services from "./components/services/Services";
-import ParallaxSection from "./components/parallax/ParallaxSection";
-import { ParallaxProvider } from "react-scroll-parallax";
 import Mercados from "./components/mercados/Mercados";
-import VerticalFeatureRow from "./components/mercados/VerticalFeatureRow";
 import MercadosSideEffect from "./components/mercados/MercadosSideEffect";
 
 export default function Home() {
   return (
     <main>
-      <ParallaxProvider>
         <Navbar />
         <CarouselHome />
         <Services />
        
         <MercadosSideEffect />
         <Mercados />
-
+{/*
         <ParallaxSection src={"/images/Detalle_vertical.webp"}>
           <></>
         </ParallaxSection>
 
-        {/*<div className="w-full h-[50svh] overflow-hidden object-fit">
+        <div className="w-full h-[50svh] overflow-hidden object-fit">
         <Image 
           alt='MBA Ingeniería Home'
           src='/images/Detalle.jpeg'
@@ -32,7 +28,6 @@ export default function Home() {
           width={1000}
         />
   </div>*/}
-      </ParallaxProvider>
     </main>
   );
 }
